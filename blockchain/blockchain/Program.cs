0133e1780c111fb88c635e233ec37a78b04d1e1d
Arnaud Vanderschrieck <arnaud.vanderschrieck@gmail.com>
2024-08-05 10:21:01 +0200

    Adding Block class

---
+++ blockchain/blockchain/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Block
+{
+    public int Index { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string PreviousHash { get; set; }
+    public string Hash { get; set; }
+    public string Data { get; set; }
+
+    public Block(int index, string previousHash, string data)
+    {
+        Index = index;
+        Timestamp = DateTime.Now;
+        PreviousHash = previousHash;
+        Data = data;
+        Hash = CalculateHash();
+    }
+
+    public string CalculateHash()
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            string rawData = Index + Timestamp + PreviousHash + Data;
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
