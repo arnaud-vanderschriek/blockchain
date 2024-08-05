a9b7035600c98b12fc400e9679cf9148a7ed9f3d
Arnaud Vanderschrieck <arnaud.vanderschrieck@gmail.com>
2024-08-05 11:32:20 +0200

    Fix indent

--- blockchain/blockchain/Blockchain.cs
+++ blockchain/blockchain/Blockchain.cs
@@ -3,51 +3,50 @@ using System.Collections.Generic;
 
 namespace Blockchain
 {
-public class Blockchain
-{
-    public List<Block> Chain { get; private set; }
-
-    public Blockchain()
+    public class Blockchain
     {
-        Chain = new List<Block> { CreateGenesisBlock() };
-    }
+        public List<Block> Chain { get; private set; }
 
-    private Block CreateGenesisBlock()
-    {
-        return new Block(0, "0", "Genesis Block");
-    }
-
-    public Block GetLatestBlock()
-    {
-        return Chain[Chain.Count - 1];
-    }
-
-    public void AddBlock(Block newBlock, int difficulty)
-    {
-        newBlock.PreviousHash = GetLatestBlock().Hash;
-        newBlock.MineBlock(difficulty);
-        Chain.Add(newBlock);
-    }
-
-    public bool IsChainValid()
-    {
-        for (int i = 1; i < Chain.Count; i++)
+        public Blockchain()
         {
-            Block currentBlock = Chain[i];
-            Block previousBlock = Chain[i - 1];
-
-            if (currentBlock.Hash != currentBlock.CalculateHash())
-            {
-                return false;
-            }
-
-            if (currentBlock.PreviousHash != previousBlock.Hash)
-            {
-                return false;
-            }
+            Chain = new List<Block> { CreateGenesisBlock() };
         }
-        return true;
-    }
-}
 
+        private Block CreateGenesisBlock()
+        {
+            return new Block(0, "0", "Genesis Block");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block newBlock, int difficulty)
+        {
+            newBlock.PreviousHash = GetLatestBlock().Hash;
+            newBlock.MineBlock(difficulty);
+            Chain.Add(newBlock);
+        }
+
+        public bool IsChainValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
